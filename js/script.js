1636e9eb471a72f4675ba663e52aa6d341a0bfbc
Alex <31960676+kim4a@users.noreply.github.com>
2018-04-15 19:54:52 -0400

    fixes
    add dropdown to menubar
    add cover page
    add missing info
    fix dropdown element width

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 window.onload = function(e) {
+	window.addEventListener('resize', fixMenuBar);
+	document.getElementById('menubartoggle').addEventListener('click', function() { toggleMenuBar(null); });
+	fixMenuBar();
+}
+
+function fixMenuBar() {
+	if (window.innerWidth > 1000) {
+		toggleMenuBar(true);
+	}
+	else {
+		toggleMenuBar(false);
+	}
+}
+
+function toggleMenuBar(close) {
+	var menubaritems = document.getElementsByClassName('menubaritem');
+	if (close || close == null && menubaritems[1].style.display == "none") {
+		for (var i = 1; i < menubaritems.length; i++) {
+			menubaritems[i].style.display = "inline-block";
+		}
+	}
+	else {
+		for (var i = 1; i < menubaritems.length; i++) {
+			menubaritems[i].style.display = "none";
+		}
+	}
 }
